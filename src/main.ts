64276265a5013d015f1b1f51f88262faa589e376
Izaiah <iilozano@ucsc.edu>
2024-11-07 11:41:31 -0800

    L16 Step Complete - Simple button implemented

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 // todo
+const button = document.createElement("button");
+button.textContent = " Click";
+document.body.appendChild(button);
+button.addEventListener("click", () => {
+  alert("You did it!");
+});
